fba3e4d2b49572207953f66eb8d308a2b95d6b0f
Jackmc <jackmc1122@gmail.com>
2019-09-09 14:12:35 -0700

    Connect to postgres and add a tweet

--- test/app.test.js
+++ test/app.test.js
@@ -2,19 +2,30 @@ require('dotenv').config();
 
 const request = require('supertest');
 const app = require('../lib/app');
-const connect = require('../lib/utils/connect');
-const mongoose = require('mongoose');
+const client = require('../lib/utils/client');
+const child_process = require('child_process');
 
 describe('app routes', () => {
-  beforeAll(() => {
-    connect();
-  });
-
   beforeEach(() => {
-    return mongoose.connection.dropDatabase();
+    child_process.execSync('npm run recreate-tables');
   });
 
   afterAll(() => {
-    return mongoose.connection.close();
+    client.end();
+  });
+
+  it('creates a note', () => {
+    return request(app)
+      .post('/api/v1/tweets')
+      .send({ title: 'my tweet', body: 'this is a great tweet', author: 'jack' })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: 1,
+          author: 'jack',        
+          title: 'my tweet',
+          body: 'this is a great tweet',
+          created: expect.any(String),
+        });
+      });
   });
 });
